20a48879d85b530a11b3fd5ec9417610ef89a2d3
Andres Gomez <agomez.vg@gmail.com>
2021-01-25 12:52:09 -0300

    Productos y categorias

---
+++ server/models/categoria.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const Schema = mongoose.Schema;
+
+let categoriaSchema = new Schema({
+    descripcion: { type: String, unique: true, required: [true, 'la descripcion es obligatoria'] },
+    usuario: { type: Schema.Types.ObjectId, ref: 'Usuario' }
+});
+
+module.exports = mongoose.model('Categoria', categoriaSchema);
